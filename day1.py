6fb6427a1ac242a633a8e59cac5347585f94ca5e
Laura Dias <ldias@sangoma.com>
2022-10-19 02:19:58 -0300

    first commit

---
+++ day1.py
@@ -0,0 +1,4 @@
+city = input("Onde você nasceu? ")
+pet = input("Qual o nome do seu pet? ")
+
+print(city + " " + pet)
---
+++ day2.py
@@ -0,0 +1,12 @@
+print("Welcome to the tip calculator.")
+bill = input("What was the total bill? $")
+tip = input("What percentage tip would you like to give? 10, 12, or 15? ")
+num_people = input("How many people to split the bill? ")
+
+tip_perc = int(tip) / 100
+tip_value = tip_perc * float(bill)
+total_bill = tip_value + float(bill)
+
+split_value = round(total_bill / int(num_people))
+
+print(f"Each person should pay: ${split_value}")
