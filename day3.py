586241928f2b436a5dfa1b2f7d13c67c8296deaa
Laura Dias <ldias@sangoma.com>
2022-10-19 15:44:16 -0300

    day 3 project: treasure island

---
+++ day3.py
@@ -0,0 +1,52 @@
+### 100 days of code (Python) - Treasure Island
+
+## Learnings:
+
+# Escape Characters
+# How to lower strings -> lower()
+# Nested if statements
+# Logical operators
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+crossroad_question = input('You\'re at a cross road. Where do you want to go? Type "left" or "right".') 
+
+if crossroad_question.lower() == "left":
+    swim_question = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.')
+    if swim_question.lower() == "wait":
+        door_question = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
+        if door_question.lower() == "yellow":
+            print("You found the treasure! You Win!")
+        elif door_question.lower() == "red":
+            print("It's a room full of fire. Game Over.")
+        elif door_question.lower() == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
