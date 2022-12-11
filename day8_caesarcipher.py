790f1f7a495459ccd61decc071ed4e8a2341338d
Laura Dias <ldias@sangoma.com>
2022-12-11 15:48:41 -0300

    latest days

---
+++ day8_caesarcipher.py
@@ -0,0 +1,54 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(msg, s):
+  encoded_text = ""
+  for letter in msg:
+    new_position = alphabet.index(letter) + s
+    final_letter = alphabet[new_position]
+    encoded_text += final_letter
+  print(f"The encoded text is {encoded_text}")
+
+def decrypt(msg, s):
+  decoded_text = ""
+  for letter in msg:
+    new_position = alphabet.index(letter) - s
+    final_letter = alphabet[new_position]
+    decoded_text += final_letter
+  print(f"The decoded text is {decoded_text}")
+
+if direction == "encode":
+  encrypt(msg=text, s=shift)
+else:
+  decrypt(msg=text, s=shift)
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+
+#TODO-4: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+
+  #TODO-5: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-6: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
