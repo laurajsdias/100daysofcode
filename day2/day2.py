4a71d1d346e5f44057a6ab888de415fd6e246098
Laura Dias <ldias@sangoma.com>
2022-12-20 08:49:28 -0300

    day 2: data types and mathematical operations

--- day2/day2.py
+++ day2/day2.py
@@ -1,3 +1,5 @@
+# Day 2 - Data types and mathematical operations
+
 print("Welcome to the tip calculator.")
 bill = input("What was the total bill? $")
 tip = input("What percentage tip would you like to give? 10, 12, or 15? ")
